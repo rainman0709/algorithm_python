2bde5b218a934f154d792ccd47491972567f0ba3
rainman <jiangpeng0614@163.com>
2018-12-04 17:34:59 +0800

    push test

--- LinkList/LinkList.py
+++ LinkList/LinkList.py
@@ -3,9 +3,9 @@
 
 
 class Node(object):
-    def __init__(self, val, p=0):
+    def __init__(self, val, pnext=None):
         self.data = val
-        self.next = p
+        self.next = pnext
 
 
 class LinkList(object):
@@ -13,12 +13,27 @@ class LinkList(object):
         self.head = 0
 
     def __getitem__(self, key):
-        pass
+        if self.is_empty():
+            print('LinkList is empty!')
+            return
+        elif key < 0 or key > self.getlength():
+            print('the key is error!')
+            return
+        else:
+            self.getitem(key)
 
     def __setitem__(self, key, value):
-        pass
+        if self.getlength() == 0:
+            print('LinkList is empty!')
+            return
+        elif key < 0 or key > self.getlength():
+            print('the key is error!')
+            return
+        else:
+            self.delete(key)
+            return self.insert(key)
 
-    def initlist(self,data):
+    def initlist(self, data):
         self.head = Node(data[0])
 
         p = self.head
@@ -31,29 +46,44 @@ class LinkList(object):
     def getlength(self):
         p = self.head
         length = 0
-        while p != 0:
+        while p is not None:
             length += 1
             p = p.next
 
         return length
 
     def is_empty(self):
-        pass
+        if self.getlength() == 0:
+            return True
+        else:
+            return False
 
     def clear(self):
-        pass
+        self.head = 0
 
-    def append(self):
-        pass
+    def append(self, item):
+        node = Node(item)
+        if not self.head:
+            self.head = node
+        else:
+            p = self.head
+            while p.next:
+                p = p.next
+            p.next = node
 
-    def getitem(self):
+    def getitem(self, index):
         pass
 
     def insert(self):
         pass
 
-    def delete(self):
-        pass
+    def delete(self, index):
+        if self.is_empty():
+            print('LinkList is empty!')
+            return
+
+        if index < 0 or index > self.getlength():
+            pass
 
     def index(self):
         pass
