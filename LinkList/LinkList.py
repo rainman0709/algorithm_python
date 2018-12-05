712526d039fc47f590bca608011d19b9548de03e
rainman <jiangpeng0614@163.com>
2018-12-05 23:15:04 +0800

    home push

--- LinkList/LinkList.py
+++ LinkList/LinkList.py
@@ -7,7 +7,8 @@ class Node(object):
     data: 节点保存的数据
     _next: 保存下一个节点对象
     '''
-    def __init__(self,data,pnext=None):
+
+    def __init__(self, data, pnext=None):
         self.data = data
         self._next = pnext
 
@@ -18,43 +19,163 @@ class Node(object):
         '''
         return str(self.data)
 
+
 class SingleList(object):
     def __init__(self):
         self.head = None
         self.length = 0
 
-    def inEmpty(self):
-        pass
+    def isEmpty(self):
+        return self.length == 0
 
-    def append(self,dataOrNode):
-        pass
+    def append(self, dataOrNode):
+        item = None
+        if isinstance(dataOrNode, Node):
+            item = dataOrNode
+        else:
+            item = Node(dataOrNode)
 
-    def delete(self,index):
-        pass
+        if not self.head:
+            self.head = item
+            self.length += 1
+        else:
+            node = self.head
+            while node._next:
+                node = node._next
+            node._next = item
+            self.length += 1
 
-    def insert(self,index,dataOrNode):
-        pass
+    def delete(self, index):
+        if self.isEmpty():
+            print('the list is empty!')
+            return
 
-    def update(self,index,data):
-        pass
+        if index < 0 or index >= self.length:
+            print('the index is error!')
+            return
 
-    def getItem(self,index):
-        pass
+        if index == 0:
+            self.head = self.head._next
+            self.length -= 1
+            return
 
-    def getIndex(self,data):
-        pass
+        j = 0
+        node = self.head
+        prev = self.head
+        while node._next and j < index:
+            prev = node
+            node = node._next
+            j += 1
+
+        if j == index:
+            prev._next = node._next
+            self.length -= 1
+
+    def insert(self, index, dataOrNode):
+        if self.isEmpty():
+            print('the list is empty')
+            return
+
+        if index < 0 or index >= self.length:
+            print('error: out of index')
+            return
+
+        item = None
+        if isinstance(dataOrNode,Node):
+            item = dataOrNode
+        else:
+            item = Node(dataOrNode)
+
+        if index == 0:
+            item._next = self.head
+            self.head = item
+            self.length += 1
+            return
+
+        j = 0
+        node = self.head
+        prev = self.head
+        while node._next and j < index:
+            prev = node
+            node = node._next
+            j += 1
+
+        if j == index:
+            item._next = node
+            prev.next = item
+            self.length += 1
+
+    def update(self, index, data):
+        if self.isEmpty() or index < 0 or index >= self.length:
+            print('error:out of index')
+            return
+
+        j = 0
+        node = self.head
+        while node._next and j < index:
+            node = node._next
+            j += 1
+
+        if j == index:
+            node.data = data
+
+    def getItem(self, index):
+        if self.isEmpty() or index < 0 or index >= self.length:
+            print('error:out of index')
+            return
+
+        j = 0
+        node = self.head
+        while node._next and j < index:
+            node = node._next
+            j += 1
+
+        if j == index:
+            return node.data
+
+    def getIndex(self, data):
+        j = 0
+        if self.isEmpty():
+            print('the list is empty!')
+            return
+
+        node = self.head
+        while node:
+            if node.data == data:
+                return j
+            node = node._next
+            j += 1
+
+        if j == self.length:
+            print('%s is not found!' % str(data))
+            return
 
     def clear(self):
-        pass
+        self.head = None
+        self.length = 0
 
     def __repr__(self):
-        pass
+        if self.isEmpty():
+            return 'empty list'
+
+        node = self.head
+        nlist = ''
+        while node:
+            nlist += str(node.data) + ''
+            node = node._next
+        return nlist
 
     def __getitem__(self, ind):
-        pass
+        if self.isEmpty() or ind < 0 or ind >= self.length:
+            print('error: out of index')
+            return
+        return self.getItem(ind)
 
-    def __setitem__(self, ind, value):
-        pass
+    def __setitem__(self, ind, val):
+        if self.isEmpty() or ind < 0 or ind >= self.length:
+            print('error: out of index')
+            return
+        self.update(ind, val)
 
     def __len__(self):
         return self.length
